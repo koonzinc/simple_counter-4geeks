0c80636691d73696dc1a8771297bde03ce057ee9
koonzinc <koonz.eth@gmail.com>
2022-09-02 18:55:05 -0400

    update

---
+++ src/components/Counter.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const Counter = () => {
+  return (
+    <div className='container'>
+        <div className='row'>
+            <div className='col-sm'>
+                One of 7 columns
+            </div>
+            <div className='col-sm'>
+                One of 7 columns
+            </div>
+            <div className='col-sm'>
+                One of 7 columns
+            </div>
+            <div className='col-sm'>
+                One of 7 columns
+            </div>
+            <div className='col-sm'>
+                One of 7 columns
+            </div>
+            <div className='col-sm'>
+                One of 7 columns
+            </div>
+            <div className='col-sm'>
+                One of 7 columns
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Counter

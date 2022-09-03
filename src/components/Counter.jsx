55548c60eed12e483fe664e2f339dd17197d4c42
koonzinc <koonz.eth@gmail.com>
2022-09-02 21:33:54 -0400

    finish

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -10,13 +10,46 @@ const Counter = () => {
     const [months, setMonths] = useState(0);
     
     
-
+/*
     useEffect(() => {
         const secondInterval = setInterval(() => {
             setSeconds(seconds => seconds + 1);
         }, 1000);
         return () => clearInterval(secondInterval);
     }, []);
+*/
+
+    useEffect(() => {
+        const secondInterval = setInterval(() => {
+            setSeconds(seconds+1)
+            if(seconds===60){
+                setSeconds(0);
+                setMinutes(minutes+1);
+            }
+            if(minutes===60){
+                setMinutes(0);
+                setHours(hours+1);
+            }
+            if(hours===24){
+                setHours(0);
+                setDays(days+1);
+            }
+            if(days===7){
+                setDays(0);
+                setWeeks(weeks+1);
+            }
+            if(weeks===4){
+                setWeeks(0);
+                setMonths(months+1);
+            }
+            if(months===12){
+                setMonths(0);
+            }
+        }, 1000);
+        return() => {
+            clearInterval(secondInterval)
+        }
+    })
 
 
 
@@ -30,32 +63,32 @@ const Counter = () => {
             </div>
             <div className='col-sm'>
                 <div className='bg-secondary py-3 font-weight-bold' style={{height: '160px', borderRadius: '16px'}}>
-                    {months >= 12 ? setMonths(0) : months}
+                    {months}
                 </div>
             </div>
             <div className='col-sm'>
                 <div className='bg-secondary py-3 font-weight-bold' style={{height: '160px', borderRadius: '16px'}}>
-                    {weeks >= 4 ? setWeeks(0) : weeks}
+                    {weeks}
                 </div>
             </div>
             <div className='col-sm'>
                 <div className='bg-secondary py-3 font-weight-bold' style={{height: '160px', borderRadius: '16px'}}>
-                    {days >= 7 ? setDays(0) : days}
+                    {days}
                 </div>
             </div>
             <div className='col-sm'>
                 <div className='bg-secondary py-3 font-weight-bold' style={{height: '160px', borderRadius: '16px'}}>
-                    {hours >= 24 ? setHours(0) : hours}
+                    {hours}
                 </div>
             </div>
             <div className='col-sm'>
                 <div className='bg-secondary py-3 font-weight-bold' style={{height: '160px', borderRadius: '16px'}}>
-                    {minutes >= 60 ? setMinutes(0) : minutes}
+                    {minutes}
                 </div>
             </div>
             <div className='col-sm'>
                 <div className='bg-secondary py-3 font-weight-bold' style={{height: '160px', borderRadius: '16px'}}>
-                    {seconds >= 10 ? setSeconds(0) : seconds}
+                    {seconds}
                 </div>
             </div>
            
